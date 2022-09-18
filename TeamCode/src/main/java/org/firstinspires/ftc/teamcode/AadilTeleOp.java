a9ebe79cfb435a3b75ab05c1fd044908e4e7f623
Dillllly <aadilbar2008@gmail.com>
2022-09-17 21:33:42 -0500

    AadilTeleOp 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadilTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadilTeleOp.java
@@ -1,20 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "AadilTeleOp", group = "TeleOp")
-public class AadilTeleOp extends LinearOpMode {
+public class AadilTeleOp extends OpMode {
 
     private DcMotor motorFrontLeft;
     private DcMotor motorBackLeft;
     private DcMotor motorFrontRight;
     private DcMotor motorBackRight;
-    @Override
-    public void runOpMode() throws InterruptedException {
 
+    @Override
+    public void init() {
         motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
         motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
         motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
@@ -23,16 +23,16 @@ public class AadilTeleOp extends LinearOpMode {
         motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        waitForStart();
+    }
 
-        while(opModeIsActive()){
+    @Override
+    public void loop() {
 
-            motorFrontLeft.setPower(gamepad1.left_stick_y);
-            motorBackLeft.setPower(gamepad1.left_stick_y);
-            motorFrontRight.setPower(gamepad1.left_stick_y);
-            motorBackRight.setPower(gamepad1.left_stick_y);
+        motorFrontLeft.setPower(gamepad1.left_stick_y);
+        motorBackLeft.setPower(gamepad1.left_stick_y);
+        motorFrontRight.setPower(gamepad1.left_stick_y);
+        motorBackRight.setPower(gamepad1.left_stick_y);
 
-            idle();
-        }
+
     }
 }
