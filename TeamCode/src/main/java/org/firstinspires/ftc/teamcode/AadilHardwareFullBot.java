55f473526e5a418b981f47b8aaafd7c9770b1bf2
Dillllly <aadilbar2008@gmail.com>
2022-09-20 14:37:39 -0500

    Working TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadilHardwareFullBot.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class AadilHardwareFullBot {
+
+    public DcMotorEx front_left;
+    public DcMotorEx front_right;
+    public DcMotorEx back_left;
+    public DcMotorEx back_right;
+
+    HardwareMap hwMap;
+
+    public AadilHardwareFullBot(){
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+        hwMap=ahwMap;
+
+        // Define and Initialize Motors
+        front_left  = hwMap.get(DcMotorEx.class, "front_left");
+        front_right = hwMap.get(DcMotorEx.class, "front_right");
+        back_left   = hwMap.get(DcMotorEx.class, "back_left");
+        back_right  = hwMap.get(DcMotorEx.class, "back_right");
+
+        front_left.setDirection(DcMotorEx.Direction.REVERSE);
+        front_right.setDirection(DcMotorEx.Direction.FORWARD);
+        back_left.setDirection(DcMotorEx.Direction.REVERSE);
+        back_right .setDirection(DcMotorEx.Direction.FORWARD);
+
+        front_left.setPower(0);
+        front_right.setPower(0);
+        back_left.setPower(0);
+        back_right.setPower(0);
+
+
+        front_left.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        front_right.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        back_left.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        back_right.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+
+        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadilTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadilTeleOp.java
@@ -3,36 +3,80 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "AadilTeleOp", group = "TeleOp")
+@TeleOp(name = "AadilTeleOp")
 public class AadilTeleOp extends OpMode {
 
-    private DcMotor motorFrontLeft;
-    private DcMotor motorBackLeft;
-    private DcMotor motorFrontRight;
-    private DcMotor motorBackRight;
+    AadilHardwareFullBot Robot = new AadilHardwareFullBot();
 
     @Override
     public void init() {
-        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
 
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        Robot.init(hardwareMap);
 
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
     }
+
+
+    @Override
+    public void init_loop() {
+        Robot.front_left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Robot.front_right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Robot.back_left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Robot.back_right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+
+    @Override
+    public void start() {
+    }
+
 
     @Override
     public void loop() {
 
-        motorFrontLeft.setPower(gamepad1.left_stick_y);
-        motorBackLeft.setPower(gamepad1.left_stick_y);
-        motorFrontRight.setPower(gamepad1.left_stick_y);
-        motorBackRight.setPower(gamepad1.left_stick_y);
+        float turnPower = -gamepad1.right_stick_x;
+        float forwardPower = -gamepad1.left_stick_y;
+        float strafePower = gamepad1.left_stick_x;
 
+        int maxSpeed = 1;
+        if (Math.abs(gamepad1.right_stick_x) > 0.1) {
 
+            Robot.front_left.setPower(-turnPower * maxSpeed);
+            Robot.front_right.setPower(turnPower * maxSpeed);
+            Robot.back_left.setPower(-turnPower * maxSpeed);
+            Robot.back_right.setPower(turnPower * maxSpeed);
+        } else {
+            Robot.front_left.setPower(0);
+            Robot.front_right.setPower(0);
+            Robot.back_left.setPower(0);
+            Robot.back_right.setPower(0);
+        }
+
+        if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+            // Forward (Left stick Y)
+            Robot.front_left.setPower(forwardPower * maxSpeed);
+            Robot.front_right.setPower(forwardPower * maxSpeed);
+            Robot.back_left.setPower(forwardPower * maxSpeed);
+            Robot.back_right.setPower(forwardPower * maxSpeed);
+        } else {
+            Robot.front_left.setPower(0);
+            Robot.front_right.setPower(0);
+            Robot.back_left.setPower(0);
+            Robot.back_right.setPower(0);
+        }
+        if (Math.abs(gamepad1.left_stick_x) > 0.1) {
+            // Strafe (Left stick X)
+            Robot.front_left.setPower(strafePower * maxSpeed);
+            Robot.front_right.setPower(-strafePower * maxSpeed);
+            Robot.back_left.setPower(-strafePower * maxSpeed);
+            Robot.back_right.setPower(strafePower * maxSpeed);
+        } else {
+            Robot.front_left.setPower(0);
+            Robot.front_right.setPower(0);
+            Robot.back_left.setPower(0);
+            Robot.back_right.setPower(0);
+        }
     }
 }
